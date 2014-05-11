1551cc4c6ece56964fbd04bdc61ec1ea6e7be83b
romasolot <romasolot@gmail.com>
2014-05-11 13:37:51 +0300

    Update .gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ function __autoload($classname){
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url'] = (((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") or (isset($_SERVER['REDIRECT_HTTPS']) && $_SERVER['REDIRECT_HTTPS'] == "on")) ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 
 /*
 |--------------------------------------------------------------------------
--- index.php
+++ index.php
@@ -18,14 +18,14 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	switch (dirname(__FILE__)){
-		case 'C:\wamp\www\calendar':
-			define('ENVIRONMENT', 'development');
-		break;
-		default:
-			define('ENVIRONMENT','production');
-		break;
-	}
+if( $_SERVER['HTTP_HOST'] == 'www.hashplan.dev' || $_SERVER['HTTP_HOST'] == 'hashplan.dev' || dirname( __FILE__ ) == 'C:\wamp\www\calendar' )
+{
+	define( 'ENVIRONMENT', 'development' );
+}
+elseif( $_SERVER['HTTP_HOST'] == 'www.hashplan.com' || $_SERVER['HTTP_HOST'] == 'hashplan.com' || $_SERVER['HTTP_HOST'] == '54.187.99.182' )
+{
+	define( 'ENVIRONMENT', 'production' );
+}
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -34,25 +34,21 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-
-if (defined('ENVIRONMENT'))
+if( defined( 'ENVIRONMENT' ) )
 {
-	switch (ENVIRONMENT)
+	switch( ENVIRONMENT )
 	{
 		case 'development':
-			error_reporting(E_ALL);
-		break;
-	
+			error_reporting( E_ALL );
+			break;
 		case 'testing':
 		case 'production':
-			error_reporting(0);
-		break;
-
+			error_reporting( 0 );
+			break;
 		default:
-			exit('The application environment is not set correctly.');
+			exit( 'The application environment is not set correctly.' );
 	}
 }
-
 /*
  *---------------------------------------------------------------
  * SYSTEM FOLDER NAME
@@ -63,8 +59,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
-
+$system_path = 'system';
 /*
  *---------------------------------------------------------------
  * APPLICATION FOLDER NAME
@@ -79,8 +74,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
-
+$application_folder = 'application';
 /*
  * --------------------------------------------------------------------
  * DEFAULT CONTROLLER
@@ -101,17 +95,13 @@ if (defined('ENVIRONMENT'))
  * Un-comment the $routing array below to use this feature
  *
  */
-	// The directory name, relative to the "controllers" folder.  Leave blank
-	// if your controller is not in a sub-folder within the "controllers" folder
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  Mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
-
+// The directory name, relative to the "controllers" folder.  Leave blank
+// if your controller is not in a sub-folder within the "controllers" folder
+// $routing['directory'] = '';
+// The controller class file name.  Example:  Mycontroller
+// $routing['controller'] = '';
+// The controller function you wish to be called.
+// $routing['function']	= '';
 /*
  * -------------------------------------------------------------------
  *  CUSTOM CONFIG VALUES
@@ -127,77 +117,60 @@ if (defined('ENVIRONMENT'))
  * Un-comment the $assign_to_config array below to use this feature
  *
  */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
-
+// $assign_to_config['name_of_config_item'] = 'value of config item';
 // --------------------------------------------------------------------
 // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
 // --------------------------------------------------------------------
-
 /*
  * ---------------------------------------------------------------
  *  Resolve the system path for increased reliability
  * ---------------------------------------------------------------
  */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (realpath($system_path) !== FALSE)
-	{
-		$system_path = realpath($system_path).'/';
-	}
-
-	// ensure there's a trailing slash
-	$system_path = rtrim($system_path, '/').'/';
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
-
+// Set the current directory correctly for CLI requests
+if( defined( 'STDIN' ) )
+{
+	chdir( dirname( __FILE__ ) );
+}
+if( realpath( $system_path ) !== false )
+{
+	$system_path = realpath( $system_path ) . '/';
+}
+// ensure there's a trailing slash
+$system_path = rtrim( $system_path, '/' ) . '/';
+// Is the system path correct?
+if( !is_dir( $system_path ) )
+{
+	exit( "Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo( __FILE__, PATHINFO_BASENAME ) );
+}
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// The PHP file extension
-	// this global constant is deprecated.
-	define('EXT', '.php');
-
-	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
-
-	// Path to the front controller (this file)
-	define('FCPATH', str_replace(SELF, '', __FILE__));
-
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
-	// The path to the "application" folder
-	if (is_dir($application_folder))
+// The name of THIS file
+define( 'SELF', pathinfo( __FILE__, PATHINFO_BASENAME ) );
+// The PHP file extension
+// this global constant is deprecated.
+define( 'EXT', '.php' );
+// Path to the system folder
+define( 'BASEPATH', str_replace( "\\", "/", $system_path ) );
+// Path to the front controller (this file)
+define( 'FCPATH', str_replace( SELF, '', __FILE__ ) );
+// Name of the "system folder"
+define( 'SYSDIR', trim( strrchr( trim( BASEPATH, '/' ), '/' ), '/' ) );
+// The path to the "application" folder
+if( is_dir( $application_folder ) )
+{
+	define( 'APPPATH', $application_folder . '/' );
+}
+else
+{
+	if( !is_dir( BASEPATH . $application_folder . '/' ) )
 	{
-		define('APPPATH', $application_folder.'/');
+		exit( "Your application folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF );
 	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
-
+	define( 'APPPATH', BASEPATH . $application_folder . '/' );
+}
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
@@ -206,7 +179,7 @@ if (defined('ENVIRONMENT'))
  * And away we go...
  *
  */
-require_once BASEPATH.'core/CodeIgniter.php';
+require_once BASEPATH . 'core/CodeIgniter.php';
 
 /* End of file index.php */
 /* Location: ./index.php */
