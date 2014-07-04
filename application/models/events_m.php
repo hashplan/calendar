3cb2b7ede1ac15f71b35e72fae993f474ad8f767
romasolot <romasolot@gmail.com>
2014-07-04 10:52:19 +0000

    Next 3,7 days starting today not tomorrow #8

--- application/models/events_m.php
+++ application/models/events_m.php
@@ -90,7 +90,7 @@ class Events_m extends MY_Model {
 				$date_range['end'] = str_replace('00:00:00', '23:59:59', $date_range['end']);
 			}
 			else if (is_numeric($options['preselects'])) {
-				$date_range['start'] = date('Y-m-d H:i:s', strtotime('tomorrow'));
+				$date_range['start'] = date('Y-m-d H:i:s', strtotime('today'));
 				$date_range['end'] = date('Y-m-d H:i:s', strtotime('+'. ($options['preselects']) .' days midnight'));
 				$date_range['end'] = str_replace('00:00:00', '23:59:59', $date_range['end']);
 			}
