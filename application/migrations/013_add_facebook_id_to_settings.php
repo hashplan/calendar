743faddaf82232ab731705ebed54c9776d696814
romasolot <romasolot@gmail.com>
2014-07-23 11:44:53 +0300

    #21 MIGRATION - facebook settings

--- application/migrations/013_add_facebook_id_to_settings.php
+++ application/migrations/013_add_facebook_id_to_settings.php
@@ -4,15 +4,22 @@ class Migration_add_facebook_id_to_settings extends CI_Migration {
     public function up() {
         $this->dbforge->add_column('user_settings', array(
             'fb_id' => array(
-                'type' => 'INTEGER',
-                'constraint' => 11,
+                'type' => 'BIGINT',
+                'constraint' => 24,
+                'null' => TRUE,
+            ),
+            'fb_email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 64,
                 'null' => TRUE,
             )
         ));
+        $this->dbforge->add_key('fb_id');
     }
 
     public function down() {
         $this->dbforge->drop_column('user_settings', 'fb_id');
+        $this->dbforge->drop_column('user_settings', 'fb_email');
     }
 
 }
