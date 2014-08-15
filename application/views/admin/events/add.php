3776bdef72bd6dce44ece4e09b028a28c28feca6
romasolot <romasolot@gmail.com>
2014-08-15 19:38:58 +0300

    #34 - edit, remove events. metroarea image

--- application/views/admin/events/add.php
+++ application/views/admin/events/add.php
@@ -2,15 +2,15 @@
     <div class="panel-heading"><h2 class="sub-header"><?= $title;?></h2></div>
     <div class="panel-body">
         <?= validation_errors('<div class="form_error_notification errors alert alert-danger" role="alert">', '</div>') ?>
-        <? $eventId = isset($event_id) && !empty($event_id)?'/'.$event_id:'';?>
-        <form action="<?= site_url('admin/events/add'.$eventId) ?>" method="POST" class="form-horizontal create-new-event-form"
+        <? $eventId = isset($event->event_id) && !empty($event->event_id)?$event->event_id:'';?>
+        <form action="<?= site_url('admin/events/add/'.$eventId) ?>" method="POST" class="form-horizontal create-new-event-form"
               role="form">
             <div class="form-group">
                 <label for="event_name" class="col-md-3 control-label"><strong>Event Name:</strong></label>
 
                 <div class="col-md-9">
                     <input type="text" class="form-control" name="name" id="event_name" placeholder="Event Name"
-                           value="<?= set_value('name', isset($name) && !empty($name)?$name:'') ?>">
+                           value="<?= set_value('name', isset($event->event_name) && !empty($event->event_name)?$event->event_name:'') ?>">
                 </div>
             </div>
             <div class="form-group">
@@ -18,7 +18,7 @@
 
                 <div class="col-md-9">
                     <textarea cols="30" rows="10" class="form-control event-description" name="description"
-                              placeholder="Description"><?= set_value('description', isset($description)&&!empty($description)?$description:'') ?></textarea>
+                              placeholder="Description"><?= set_value('description', isset($event->event_description)&&!empty($event->event_description)?$event->event_description:'') ?></textarea>
                 </div>
             </div>
             <br>
@@ -27,11 +27,11 @@
                 <label for="event_date_time" class="col-md-3 control-label"><strong>Event Date/Time:</strong></label>
 
                 <div class="col-md-5">
-                    <div class="bfh-datepicker event-date" data-date="<?=set_value('date',isset($date)&&!empty($date)?$date:'today')?>" data-min="today" data-name="date"
+                    <div class="bfh-datepicker event-date" data-date="<?=set_value('date',isset($event->date)&&!empty($event->date)?$event->date:'today')?>" data-min="today" data-name="date"
                          data-format="y-m-d"></div>
                 </div>
                 <div class="col-md-4">
-                    <div class="bfh-timepicker event-time" data-time="<?=set_value('time',isset($time)&&!empty($time)?$time:'now')?>" data-name="time"></div>
+                    <div class="bfh-timepicker event-time" data-time="<?=set_value('time',isset($event->time)&&!empty($event->time)?$event->time:'now')?>" data-name="time"></div>
                 </div>
             </div>
 
@@ -41,7 +41,7 @@
                 <div class="col-md-9">
                     <input type="text" class="form-control" name="event_booking_link" id="event_booking_link"
                            placeholder="Booking link"
-                           value="<?= set_value('event_booking_link', isset($booking_link) && !empty($booking_link)?$booking_link:'') ?>">
+                           value="<?= set_value('event_booking_link', isset($event->event_booking_link) && !empty($event->event_booking_link)?$event->event_booking_link:'') ?>">
                 </div>
             </div>
 
@@ -52,7 +52,7 @@
 
                     <div class="col-md-9">
                         <div class="input-group">
-                            <?= form_dropdown('venue_id', (array)$venues, isset($venue_id) && !empty($venue_id)?$venue_id:'');?>
+                            <?= form_dropdown('venue_id', (array)$venues, isset($venue_id) && !empty($venue_id)?$venue_id:'', 'class="form-control"');?>
                             <? if (isset($metros) && !empty($metros)): ?>
                                 <span class="input-group-btn">
                                     <select class="metro_area btn" id="metro_area_filter">
