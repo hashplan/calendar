4365112dcdb9a3f6703a895a91b160ae39376131
romasolot <romasolot@gmail.com>
2014-08-19 10:02:03 +0300

    autoloader fix

--- application/config/config.php
+++ application/config/config.php
@@ -360,15 +360,17 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
-
-function __autoload($classname){
-    if(strpos($classname,'CI_')!==0){
-        $file = APPPATH	.'libraries/' . $classname . '.php';
-        if (file_exists($file)&&is_file($file)){
-            @include_once($file);
+if(!function_exists('__autoload')){
+    function __autoload($classname){
+        if(strpos($classname,'CI_')!==0){
+            $file = APPPATH	.'libraries/' . $classname . '.php';
+            if (file_exists($file)&&is_file($file)){
+                @include_once($file);
+            }
         }
     }
 }
+
 
 /* End of file config.php */
 /* Location: ./application/config/config.php */
