60d869b397b24be38f0db15bc7562ce2ec6eb593
romasolot <romasolot@gmail.com>
2014-09-01 13:44:43 +0300

    #36 layout style fixed

--- application/views/email/activate.tpl.php
+++ application/views/email/activate.tpl.php
@@ -1,6 +1,6 @@
 <table border="0" cellspacing="0" cellpadding="0" width="549px" bgcolor="#ffffff" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;">
     <tr>
-        <td align="center" style="vertical-align:middle;background: url(<?=site_url('assets/img/homepage/beer_garden_with_boat.jpg')?>);background-color: #FCEE88 " width="549px" height="366px">
+        <td align="center" style="vertical-align:middle;background: url(<?=site_url('assets/img/homepage/beer_garden_with_boat.jpg')?>);" width="549px" height="366px">
             <a href="<?=site_url('activate/' . $id . '/' . $activation)?>" target="_blank"
                class="activate_btn"
                style="-moz-user-select: none; -ms-user-select: none; -webkit-user-select: none; background-color: #5cb85c; background-image: none; border: 1px solid transparent; border-color: #4cae4c; border-radius: 6px; bottom: 25%; color: #fff; cursor: pointer; display: inline-block; font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 18px; font-weight: 400; left: 25%; line-height: 1.33; margin-bottom: 0; padding: 10px 24px; text-align: center; text-decoration: none; user-select: none; vertical-align: middle; white-space: nowrap; margin-top: 60px;">Activate
