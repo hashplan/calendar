9eca5974e251ee9daefb6b15f12f54f7bbdc65c2
romasolot <romasolot@gmail.com>
2014-10-17 14:43:28 +0300

    #23 - proxy

--- application/libraries/crawler/src/CrawlerDrv.php
+++ application/libraries/crawler/src/CrawlerDrv.php
@@ -4,15 +4,48 @@ class CrawlerDrv
 {
     protected $formatUrl;
     protected $CI;
+    protected $proxies_list = array(
+        array(
+            'ip' => '64.31.22.131',
+            'port' => 3127
+        ),
+        array(
+            'ip' => '198.52.217.44',
+            'port' => 8089
+        ),
+        array(
+            'ip' => '199.200.120.140',
+            'port' => 7808
+        ),
+        array(
+            'ip' => '23.95.42.138',
+            'port' => 3127
+        ),
+        array(
+            'ip' => '162.208.49.45',
+            'port' => 7808
+        ),
+        array(
+            'ip' => '199.200.120.140',
+            'port' => 8089
+        ),
+        array(
+            'ip' => '162.216.155.136',
+            'port' => 8089
+        ),
+        array(
+            'ip' => '66.85.131.18',
+            'port' => 7808
+        ),
+    );
 
     public function __construct()
     {
         $this->CI = & get_instance();
     }
 
-    protected function curl($URLServer, $postdata = "", $cookieFile = null, $proxy = false, $proxyRetry = 0)
+    protected function curl($URLServer, $postdata = "", $cookieFile = null, $useProxy = false, $proxyRetry = 0)
     {
-        global $proxyCache;
         $agent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.12) Gecko/20101027 Ubuntu/10.10 (maverick) Firefox/3.6.12";
         $cURL_Session = curl_init();
 
@@ -28,36 +61,17 @@ class CrawlerDrv
             curl_setopt($cURL_Session, CURLOPT_COOKIEFILE, $cookieFile);
         }
 
-        if ($proxy == true) {
-            if ($proxyCache == "") {
-                $c = curl("http://www.xroxy.com/proxylist.htm", "", null, false);
-                preg_match_all("/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:[0-9]{1,5}/", $c, $matches);
-                echo $matches;
-                $matches = $matches[0];
-                echo $matches;
-                print_r($matches);
-                $proxyCache = $matches[rand(0, (count($matches) - 1))];
-            }
-
-            echo "proxy:$proxyCache\n";
-
-
-            list($proxy_ip, $proxy_port) = explode(":", $proxyCache);
-
-            curl_setopt($cURL_Session, CURLOPT_PROXYPORT, $proxy_port);
+        if ($useProxy == true) {
+            $proxy = $this->proxies_list[rand(0, (count($this->proxies_list) - 1))];
+            curl_setopt($cURL_Session, CURLOPT_PROXYPORT, $proxy['port']);
             curl_setopt($cURL_Session, CURLOPT_PROXYTYPE, 'HTTP');
-            curl_setopt($cURL_Session, CURLOPT_PROXY, $proxy_ip);
+            curl_setopt($cURL_Session, CURLOPT_PROXY, $proxy['ip']);
         }
 
         $result = curl_exec($cURL_Session);
 
-        if ($result === false) {
-            echo 'Curl error: ' . curl_error($cURL_Session) . "\n";
-
-            if ($proxy == true && $proxyRetry <= 5) {
-                $proxyCache = "";
-                curl($URLServer, $postdata = "", $cookieFile, $proxy, $proxyRetry++);
-            }
+        if ($result === false && $useProxy == true && $proxyRetry <= 5) {
+            $this->curl($URLServer, $postdata, $cookieFile, $useProxy, ++$proxyRetry);
         }
         curl_close($cURL_Session);
 
